022581487a7642704a1bbf3f6de5bb009e857d89
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2024-08-09 12:14:34 -0700

    Startup time data

--- benchmark-startup-time.sh
+++ benchmark-startup-time.sh
@@ -1,1 +1,3 @@
 rm startuptime.log;nvim --startuptime startuptime.log;cat startuptime.log | tail -n 2 | head -n 1
+mv startuptime.log tmp.log;cat tmp.log | sed "s/$USER/\$USER/" > startuptime.log
+rm tmp.log
